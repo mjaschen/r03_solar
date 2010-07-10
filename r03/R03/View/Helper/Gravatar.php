d7dd4eb599971f6e16075c13f3120ea5d795beec
Marcus T. Jaschen <mjaschen@gmail.com>
2010-07-10 12:28:19 +0200

    Gravatar helper added

---
+++ r03/R03/View/Helper/Gravatar.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * View helper for displaying a gravatar image
+ *
+ * @link http://en.gravatar.com/site/implement/images/
+ *
+ * @package R03
+ *
+ * @author Marcus T. Jaschen <mj@mtb-news.de>
+ **/
+class R03_View_Helper_Gravatar extends Solar_View_Helper
+{
+    /**
+     * default configuration
+     *
+     * @config string href The base URL of Gravatar web service
+     *
+     * @config string default The default icon which is shown if
+     *   no gravatar is found for the given email address
+     *
+     * @config string size The size of the Gravatar image, '1' ... '512'
+     *
+     * @config string rating The allowed rating of the returned image.
+     *   An user can choose a rating for his Gravatar images.
+     *
+     * @var array
+     **/
+    protected $_R03_View_Helper_Gravatar = array(
+        'href'    => "http://www.gravatar.com/avatar/",
+        'default' => 'mm',
+        'size'    => '80',
+        'rating'  => 'x'
+    );
+
+    /**
+     * returns an <img /> tag for a gravatar image
+     *
+     * Options:
+     *
+     * - 'default' - choose a different default image
+     * - 'size' - choose a different image size (1..512)
+     * - 'rating' - choose a different rating for the image
+     *
+     * @param string $email the email address for which the gravatar should be loaded
+     *
+     * @param array $options
+     *
+     * @return string
+     *
+     * @author Marcus T. Jaschen <mj@mtb-news.de>
+     **/
+    public function gravatar($email, $options = array())
+    {
+        $hash = md5(strtolower(trim($email)));
+        $default = $this->_config['default'];
+        $size = $this->_config['size'];
+        $rating = $this->_config['rating'];
+
+        if (!empty($options['default'])) {
+            $default = $options['default'];
+        }
+
+        if (!empty($options['size'])) {
+            $size = $options['size'];
+        }
+
+        if (!empty($options['rating'])) {
+            $rating = $options['rating'];
+        }
+
+        $url = "{$this->_config['href']}{$hash}?d={$default}&s={$size}&r={$rating}";
+
+        return $this->_view->image($url, array(
+            'width'  => $size,
+            'height' => $size,
+            'alt'    => 'avatar'
+        ));
+    }
+}
---
+++ r03/config/default.php
@@ -0,0 +1,6 @@
+<?php
+$config['Solar_View']['helper_class'][] = 'R03_View_Helper';
+
+// Gravatar View Helper
+
+$config['R03_View_Helper_Gravatar']['size'] = '32';
